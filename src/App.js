d9e5513e7282c21f55b7f21afa17bdb15fe21622
Jubair <jubairhossen55@gmail.com>
2022-04-29 11:49:39 +0600

    basic router added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import LogIn from './Pages/LogIn/LogIn';
+import SignUp from './Pages/SignUp/SignUp';
 
 function App() {
   return (
     <div className="App">
-      <h1>Client site</h1>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/login' element={<LogIn></LogIn>}></Route>
+        <Route path='/signup' element={<SignUp></SignUp>}></Route>
+      </Routes>
     </div>
   );
 }
